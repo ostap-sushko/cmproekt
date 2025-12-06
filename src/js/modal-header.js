108ab7557a639cea8e7daf53fdcd8a147d116644
matvey <matveyprys@gmail.com>
2025-12-06 12:20:11 +0200

    trying to solve problem

--- src/js/modal-header.js
+++ src/js/modal-header.js
@@ -1,1 +1,21 @@
 const closeRef = document.querySelector(".modal-header__close")
+const modalRef = document.querySelector(".modal-header")
+const inputRef = document.querySelector(".modal-header__information")
+const textRef = document.querySelector(".header__greetingsspan")
+const formRef = document.querySelector(".modal-header__form")
+
+closeRef.addEventListener("click", () => {
+    modalRef.style.display = "none"
+})
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    modalRef.style.display = "none";
+  }
+})
+
+formRef.addEventListener("submit", (event) => {
+  event.preventDefault();
+  textRef.textContent = event.currentTarget.name.value;
+  modalRef.style.display = "none";
+});

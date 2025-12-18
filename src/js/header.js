65f2b6899b77a7b39a921236653d86d8c1b40110
matvey <matveyprys@gmail.com>
2025-12-18 17:10:17 +0200

    made dark theme

--- src/js/header.js
+++ src/js/header.js
@@ -2,14 +2,26 @@ import sun from '../img/sun-slider.png';
 import night from '../img/night-slider.png';
 
 const contRef = document.querySelector('.header__containerslider');
-const boxRef = document.querySelector('.header__sliderbox');
 const sliderRef = document.querySelector('.header__slider');
+const containerRef = document.querySelectorAll('.container');
+const modalRef = document.querySelector('.modal-header');
+const allTextRef = document.querySelectorAll('h2');
 
 contRef.addEventListener('click', () => {
   contRef.classList.toggle('active-slider');
   if (contRef.classList.contains('active-slider')) {
     sliderRef.src = night;
+    document.body.style.backgroundColor = '#333';
+    modalRef.style.backgroundColor = '#444';
+
+    containerRef.forEach(el => el.classList.add('dark-mode'));
+    allTextRef.forEach(el => (el.style.color = '#FFFFFF'));
   } else {
     sliderRef.src = sun;
+    document.body.style.backgroundColor = '#FFFFFF';
+    modalRef.style.backgroundColor = '#FFFFFF';
+
+    containerRef.forEach(el => el.classList.remove('dark-mode'));
+    allTextRef.forEach(el => (el.style.color = '#000'));
   }
 });

bf4a3b47fd25711c48993fce0c3ac6552b27e642
Vova-Kachur <kachurslavik123@gmail.com>
2025-12-20 12:53:11 +0200

    madefooterandmodal

--- src/js/calculatetime.js
+++ src/js/calculatetime.js
@@ -3,28 +3,43 @@ const form = document.querySelector('.calculatetime__form');
 form.addEventListener('submit', event => {
   event.preventDefault();
   const ms = event.currentTarget.elements.name.value.trim();
-  const time = fromMilliseconds(ms);
+  const time = fromMinutes(ms);
   renderTime(time);
 });
 console.log(form);
 
-function renderTime({ days, hours, minutes, seconds }) {
-  return (resultTime.textContent = `${days} дн. ${hours}:${minutes}:${
-    seconds < 10 ? '0' + seconds : seconds
-  }`);
-}
+function renderTime({ days, hours, minutes }) {
+  return (resultTime.textContent = `${days} дн. ${hours}:${minutes}`)}
 
-function fromMilliseconds(ms) {
-  const days = Math.floor(ms / (1000 * 60 * 60 * 24));
-  ms %= 1000 * 60 * 60 * 24;
+function fromMinutes(totalMinutes) {
+  const days = Math.floor(totalMinutes / (60 * 24));
+  totalMinutes %= 60 * 24;
 
-  const hours = Math.floor(ms / (1000 * 60 * 60));
-  ms %= 1000 * 60 * 60;
+  const hours = Math.floor(totalMinutes / 60);
+  const minutes = totalMinutes % 60;
 
-  const minutes = Math.floor(ms / (1000 * 60));
-  ms %= 1000 * 60;
-
-  const seconds = Math.floor(ms / 1000);
+  const seconds = 0; // бо початкове значення у хвилинах
 
   return { days, hours, minutes, seconds };
 }
+
+
+
+
+
+
+
+// function fromMilliseconds(ms) {
+//   const days = Math.floor(ms / (1000 * 60 * 60 * 24));
+//   ms %= 1000 * 60 * 60 * 24;
+
+//   const hours = Math.floor(ms / (1000 * 60 * 60));
+//   ms %= 1000 * 60 * 60;
+
+//   const minutes = Math.floor(ms / (1000 * 60));
+//   ms %= 1000 * 60;
+
+//   const seconds = Math.floor(ms / 1000);
+
+//   return { days, hours, minutes, seconds };
+// }

df35deea5e3c919356362f3a6dd64edbfc467b31
ostap-sushko <sushkoostap@gmail.com>
2025-12-21 13:19:23 +0200

    made-number

---
+++ src/js/guessnumber.js
@@ -0,0 +1,19 @@
+const textRef = document.querySelector('.guessnumber__identifyyear');
+const inputRef = document.querySelector('.guessnumber__inputyear');
+const formRef = document.querySelector('.guessnumber__form');
+
+formRef.addEventListener('submit', e => {
+  e.preventDefault();
+const num = Math.floor(Math.random() * (10 - 1) + 1); 
+  const value = Number(inputRef.value);
+
+  if (value === num) {
+    textRef.textContent = `🎉Вітаю, ви вгадали число! ${num}`;
+    textRef.classList.add('yeartrue');
+    textRef.classList.remove('yearfalse');
+  } else {
+    textRef.textContent = '❌ Неправильне число';
+    textRef.classList.add('yearfalse');
+    textRef.classList.remove('yeartrue');
+  }
+});
